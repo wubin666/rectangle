62111474d687b90f8d5aa4b573ef100552c7a830
wangding <408542507@qq.com>
2019-03-18 18:59:53 +0800

    v0.1 code new

--- rectangle.js
+++ rectangle.js
@@ -1,17 +1,18 @@
-$(function){
-     var $width=$("#width"),
-         $height=$("#height"),
-         $btnCal=$("#calculate"),
-         $perimeter=$("#perimeter"),
-         $area=$("#area");
+$(function() {
+     var $width = $('#width'),
+         $height = $('#height'),
+         $btnCal = $('#calculate'),
+         $perimeter = $('#perimeter'),
+         $area = $('#area');
 
-     $btnCal.click(function(){
-        var w=Number($width.val()),
-            h=Number($height.val());
+      $btnCal.click(function(){
+         var w = Number($width.val()),
+             h = Number($height.val());
 
-        var p=2+(w+h),
-            a=w*h;
-        $perimeter.val(p);
-        $area.val(a);
-  });
+         var p = 2 * (w + h),
+             a = w * h;
+
+         $perimeter.val(p);
+         $area.val(a);   
+      });
 });
