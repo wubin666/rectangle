8f22421ed702e57d7c4b4949139bed057ed7ffe3
wangding <408542507@qq.com>
2019-03-18 18:45:34 +0800

    v0.1 code gai

--- rectangle.js
+++ rectangle.js
@@ -3,10 +3,12 @@ $(function){
          $height=$("#height"),
          $btnCal=$("#calculate"),
          $perimeter=$("#perimeter"),
-         $area=$("#area")
+         $area=$("#area");
+
      $btnCal.click(function(){
         var w=Number($width.val()),
             h=Number($height.val());
+
         var p=2+(w+h),
             a=w*h;
         $perimeter.val(p);
